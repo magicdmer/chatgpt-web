00ade1a3b55d44e5a069f0b545e090bcacffa128
magicdmer <magicdmer@163.com>
2024-03-26 15:37:01 +0800

    添加零一万物大模型支持

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -100,6 +100,10 @@ export async function initApi(key: KeyConfig, chatModel: CHATMODEL) {
       options.maxModelTokens = 6144
       options.maxResponseTokens = 1024
     }
+    else if (model.toLowerCase().includes('yi-34b-chat-200k')) {
+      options.maxModelTokens = 204800
+      options.maxResponseTokens = 4096
+    }
     else {
       // If none of the above, use the default values, set the maxModelTokens and maxResponseTokens to 8192 and 2048 respectively
       options.maxModelTokens = 4096
