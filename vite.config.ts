f68075ebd22991072fd3c7df0f8d8a9950987c6d
magicdmer <magicdmer@163.com>
2026-06-10 23:12:02 +0800

    chore: 修改网站图标和PWA标题

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ function setupPlugins(env: ImportMetaEnv): PluginOption[] {
     env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
       injectRegister: 'auto',
       manifest: {
-        name: 'chatGPT',
-        short_name: 'chatGPT',
+        name: '简聊',
+        short_name: '简聊',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
